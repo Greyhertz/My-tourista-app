c6b29f5c820dbf630befc202e2cd1522ff9e086a
devodii <emmanuelodii80@gmail.com>
2025-08-13 18:03:49 +0100

    feat: added example button usage in test page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Test() {
-  return <div className="bg-background text-foreground">Test</div>;
+  return (
+    <div className="bg-background text-foreground">
+      <p className="text-med">Test</p>
+      <Button variant="default">Action</Button>
+    </div>
+  );
 }
